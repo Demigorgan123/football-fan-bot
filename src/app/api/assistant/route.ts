a007f57b4e005e5ce5325ed7f8fc2296708741ac
Demigorgan123 <rbanswal123@gmail.com>
2024-07-21 09:32:40 +0530

    change openAi name in api

--- src/app/api/assistant/route.ts
+++ src/app/api/assistant/route.ts
@@ -1,4 +1,4 @@
-import { openai } from '@/app/openai';
+import { openai } from '@/app/openAi';
 
 export async function POST() {
   const assistant = await openai.beta.assistants.create({
--- src/app/api/assistant/threads/[threadId]/messages/route.ts
+++ src/app/api/assistant/threads/[threadId]/messages/route.ts
@@ -1,4 +1,4 @@
-import { openai } from '@/app/openai';
+import { openai } from '@/app/openAi';
 import { NextRequest } from 'next/server';
 
 export async function POST(
